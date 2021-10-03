a145d937fcaf6e86317385c2c7095e2d5b76df55
Oleg Sokolovsky <oleg.sokolovsky@azati.com>
2021-10-03 10:34:22 +0300

    feat: add  function createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -10,10 +10,19 @@ import { NotImplementedError } from '../extensions/index.js';
  * @example
  * 
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
- * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
+ * reateDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+export default function createDreamTeam( members ) {
+  if (!Array.isArray(members)) {
+    return false
+  }
+  return members.filter( (elem) => { return (typeof elem == 'string')}).map( elem => elem.trim().toUpperCase()[0]).sort().join('');
+  // console.log(arr)
+
+
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
+
+// createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]);
