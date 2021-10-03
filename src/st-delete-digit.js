18054cfc344efb725ceabe890f2584edeea09169
Oleg Sokolovsky <oleg.sokolovsky@azati.com>
2021-10-04 00:42:32 +0300

    feat: add  function deleteDigit

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,7 +11,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+    let newStr = n.toString();
+    let maxNum = 0;
+    for (let i = 0; i < newStr.length; ++i) {
+        if (maxNum < +(newStr.slice(0, i) + newStr.slice(i + 1))) {
+            maxNum = +(newStr.slice(0, i) + newStr.slice(i + 1))
+        }
+    }
+    return maxNum
 }
