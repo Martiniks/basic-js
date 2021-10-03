544492260a70a882b2dfa930dbd9e0741efa593b
Oleg Sokolovsky <oleg.sokolovsky@azati.com>
2021-10-03 22:42:26 +0300

    feat: add  function isMAC48Address

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -14,7 +14,9 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-export default function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function isMAC48Address( n ) {
+  let strRegexp=/^[0-9a-fA-F]{2}(-[0-9a-fA-F]{2}){5}$/i
+  return strRegexp.test(n)
 }
+
+
