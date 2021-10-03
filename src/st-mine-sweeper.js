c6b268f2d0e324acfddde26cbe71285db949f41b
Oleg Sokolovsky <oleg.sokolovsky@azati.com>
2021-10-03 21:42:42 +0300

    feat: add  function minesweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,7 +23,54 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper(matrix) {
+    let newArr = [];
+    for (let i = 0; i < matrix.length; ++i) {
+        newArr[i] = [];
+    }
+
+    for (let i = 0; i < matrix.length; ++i) {
+        for (let j = 0; j < matrix[i].length; ++j) {
+            newArr[i].push(countMine(i, j));
+        }
+    }
+    return newArr
+
+    function countMine(x, y) {
+        let n = 0;
+
+        if ((x - 1) >= 0 && (y - 1) >= 0 && matrix[x - 1][y - 1]) {
+            n += 1
+        }
+
+        if ((y - 1) >= 0 && matrix[x][y - 1]) {
+            n += 1
+        }
+
+        if ((y - 1) >= 0 && (x + 1) < matrix.length && matrix[x + 1][y - 1]) {
+            n += 1
+        }
+
+        if ((x - 1) >= 0 && matrix[x - 1][y]) {
+            n += 1
+        }
+
+        if ((x + 1) < matrix.length && matrix[x + 1][y]) {
+            n += 1
+        }
+
+        if ((x - 1) >= 0 && matrix[x - 1][y + 1]) {
+            n += 1
+        }
+
+        if (matrix[x][y + 1]) {
+            n += 1
+        }
+
+        if ((x + 1) < matrix.length && matrix[x + 1][y + 1]) {
+            n += 1
+        }
+        return n
+    }
+
 }
